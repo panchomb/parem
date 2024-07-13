900b3f5070b5603355d6ae9fe60aa116fa497519
Jose Chachi <jose.chachi@utec.edu.pe>
2024-07-13 16:02:38 -0500

    feat: Add finder possible initial states.

--- seq.cpp
+++ seq.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 
+#include <omp.h>
+
 using namespace std;
 
 typedef long state;
@@ -13,9 +15,11 @@ state **transition_table;
 vector<state> final_states;
 string input_str;
 
+#define NUM_THR 4
+
 void define_automata() {
     state final_state;
-    freopen("afd.txt","r",stdin);
+    freopen("dfapaper.txt","r",stdin);
     cin >> num_states >> alphabet_size >> num_final_states;
 
     transition_table = new state*[num_states];
@@ -41,7 +45,69 @@ state transition(state q, char c) {
     return row[char_to_int(c)];
 }
 
+void read_table(){
+
+    for (int i = 0; i < num_states; i++) {
+        for (int j = 0; j < alphabet_size; j++) {
+            cout << transition_table[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+}
+
 state rem_parser(state q) {
+    int i = 0;
+    state j;
+    state k;
+    int start_position;
+    string pi_input;
+    omp_set_num_threads(NUM_THR);
+    
+    // Initialize final result
+    // vector<vector<string>> I = {};
+    size_t len_str = input_str.size();
+    #pragma omp parallel default(shared) private(i, j, k, pi_input, start_position)
+    {
+        //#pragma omp for schedule(dynamic) nowait
+        //#pragma omp for schedule(static,chunk) private(i) nowait
+        #pragma omp for schedule(static) nowait
+        for(int i = 0; i < NUM_THR; i++){
+            int start_position;
+            start_position = i*(len_str/NUM_THR);
+            
+            pi_input = input_str.substr(start_position, (len_str/NUM_THR));
+            cout << pi_input << "  ";
+            // cout << start_position << "  "<<omp_get_thread_num() << "\n";
+            vector<state> S(num_states,-1);
+            for(state j = 0; j < num_states; j++){ // 0 1 2 3 4
+                state* row = transition_table[j];
+                // cout << pi_input.at(0) << " ";
+                // cout << row[char_to_int(pi_input.at(0))] << " ";
+                if (row[char_to_int(pi_input.at(0))] != -1) S[j] = j;
+            }
+
+            vector<state> L(num_states,-1);
+            for(state k = 0; k < num_states; k++){ // 0 1 2 3 4
+                state* row2 = transition_table[k];
+                // cout << pi_input.at(len_str/NUM_THR - 1) << " ";
+                // cout << row[char_to_int(pi_input.at(len_str/NUM_THR - 1))] << " ";
+                if (row2[char_to_int(pi_input.at(len_str/NUM_THR -1))] != -1) L[k] = k;
+            }
+
+            // for(const auto dat: S) cout << dat << " ";
+            // cout << "\n";
+            // for(const auto dat: L) cout << dat << " ";
+            // cout << "\n";
+
+            
+        }
+        
+    }  /* end of parallel section */
+
+
+
+
     for (int i = 0; i < input_str.size(); i++) {
         if (q == -1) {
             printf("INVALID STATE\n");
@@ -49,7 +115,7 @@ state rem_parser(state q) {
         }
 
         char c = input_str[i];
-        printf("CURRENT STATE: %ld, next char: %c\n", q, c);
+        // printf("CURRENT STATE: %ld, next char: %c\n", q, c);
         q = transition(q, c);
     }
     printf("FINAL STATE: %ld\n", q);
@@ -69,9 +135,10 @@ bool match_re() {
     // }
 }
 
-int main() {
+// #define N 10
+int main(int argc, char *argv[]) {
     define_automata();
-
+    // read_table();
     cin >> input_str;
 
     if (match_re()) {
